5b0340f7c37952afbd2e212e7fad83a682caf0ad
ars1224 <126647608+ars1224@users.noreply.github.com>
2025-04-03 10:17:46 +1300

    temprature

---
+++ Temp Converter/Temo Converter.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	char unit, unit2;
+	int temp;
+
+	cout << "Enter your temperature: ";
+	cin >> temp;
+	cout << "Enter the temperature scale (C, F, K): ";
+	cin >> unit;
+	cout << "Enter the temperature scale you want to convert to (C, F, K): ";
+	cin >> unit2;
+
+	if(unit == 'c' || unit == 'C' ) {}
+	
+}
