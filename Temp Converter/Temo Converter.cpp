460b06b8eec1fbd09d9073da3cfda69c447d269e
ars1224 <a.tayao1224@gmail.com>
2025-04-03 13:14:26 +1300

    temp2

--- Temp Converter/Temo Converter.cpp
+++ Temp Converter/Temo Converter.cpp
@@ -4,15 +4,42 @@ using namespace std;
 int main() {
 
 	char unit, unit2;
-	int temp;
+	double temp;
 
-	cout << "Enter your temperature: ";
-	cin >> temp;
-	cout << "Enter the temperature scale (C, F, K): ";
-	cin >> unit;
-	cout << "Enter the temperature scale you want to convert to (C, F, K): ";
-	cin >> unit2;
-
-	if(unit == 'c' || unit == 'C' ) {}
+		cout << "Enter your temperature: ";
+		cin >> temp;
 	
+	while (true) {
+		cout << "Enter the temperature scale ( Celsius(C), Fahrenheit(F), Kelvin(K) ): ";
+		cin >> unit;
+
+		unit = toupper(unit);
+
+		if (unit != 'C' && unit != 'F' && unit != 'K') {
+			cout << "Invalid Input" << endl;
+		}
+		else {
+			break;
+		}
+	}
+	while (true) {
+		cout << "Enter the temperature scale you want to convert to (C, F, K): ";
+		cin >> unit2;
+
+		unit2 = toupper(unit2);
+
+		if (unit2 != 'C' && unit2 != 'F' && unit2 != 'K') {
+			cout << "Invalid Input" << endl;
+		}
+		else {
+			break;
+		}
+	}
+
+		if (unit == 'c' && unit2 == 'f') {
+			
+		}
+	
+
+	return 0;
 }
